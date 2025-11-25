1218c28f3abfd75ade04224ced49262c10a38eea
Matt Kotsenas <matt.kotsenas@gmail.com>
2025-11-24 16:17:52 -0800

    Lazy init GlobalBindingCache
    Saves another 10KB for a simple ParseAndAnalyze

--- src/Kusto.Language/Binder/GlobalBindingCache.cs
+++ src/Kusto.Language/Binder/GlobalBindingCache.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 namespace Kusto.Language.Binding
 {
@@ -12,22 +11,106 @@ namespace Kusto.Language.Binding
     /// </summary>
     internal class GlobalBindingCache
     {
-        internal readonly ThreadSafeDictionary<IReadOnlyList<TableSymbol>, TableSymbol> UnifiedNameColumnsMap =
-            new ThreadSafeDictionary<IReadOnlyList<TableSymbol>, TableSymbol>(ReadOnlyListComparer<TableSymbol>.Default);
+        public GlobalBindingCache()
+        {
+        }
 
-        internal readonly ThreadSafeDictionary<IReadOnlyList<TableSymbol>, TableSymbol> UnifiedNameAndTypeColumnsMap =
-            new ThreadSafeDictionary<IReadOnlyList<TableSymbol>, TableSymbol>(ReadOnlyListComparer<TableSymbol>.Default);
+        // PERF: Lazily initialize caches as they are needed (ConcurrentDictionary can be expensive to create)
+        private ThreadSafeDictionary<IReadOnlyList<TableSymbol>, TableSymbol> _unifiedNameColumnsMap;
+        private ThreadSafeDictionary<IReadOnlyList<TableSymbol>, TableSymbol> _unifiedNameAndTypeColumnsMap;
+        private ThreadSafeDictionary<IReadOnlyList<TableSymbol>, TableSymbol> _commonColumnsMap;
+        private ThreadSafeDictionary<Signature, MostRecentlyUsedCache<CallSiteInfo, FunctionCallExpansion>> _callSiteToExpansionMap;
+        private ThreadSafeDictionary<Signature, MostRecentlyUsedCache<CallSiteInfo, TypeSymbol>> _callSiteToResultTypeMap;
+        private ThreadSafeDictionary<Signature, FunctionBodyFacts> _databaseFunctionBodyFacts;
 
-        internal readonly ThreadSafeDictionary<IReadOnlyList<TableSymbol>, TableSymbol> CommonColumnsMap =
-            new ThreadSafeDictionary<IReadOnlyList<TableSymbol>, TableSymbol>(ReadOnlyListComparer<TableSymbol>.Default);
+        public ThreadSafeDictionary<IReadOnlyList<TableSymbol>, TableSymbol> UnifiedNameColumnsMap
+        {
+            get
+            {
+                if (_unifiedNameColumnsMap == null)
+                {
+                    Interlocked.CompareExchange(
+                        ref _unifiedNameColumnsMap,
+                        new ThreadSafeDictionary<IReadOnlyList<TableSymbol>, TableSymbol>(ReadOnlyListComparer<TableSymbol>.Default),
+                        null);
+                }
+                return _unifiedNameColumnsMap;
+            }
+        }
 
-        internal ThreadSafeDictionary<Signature, MostRecentlyUsedCache<CallSiteInfo, FunctionCallExpansion>> CallSiteToExpansionMap =
-            new ThreadSafeDictionary<Signature, MostRecentlyUsedCache<CallSiteInfo, FunctionCallExpansion>>();
+        public ThreadSafeDictionary<IReadOnlyList<TableSymbol>, TableSymbol> UnifiedNameAndTypeColumnsMap
+        {
+            get
+            {
+                if (_unifiedNameAndTypeColumnsMap == null)
+                {
+                    Interlocked.CompareExchange(
+                        ref _unifiedNameAndTypeColumnsMap,
+                        new ThreadSafeDictionary<IReadOnlyList<TableSymbol>, TableSymbol>(ReadOnlyListComparer<TableSymbol>.Default),
+                        null);
+                }
+                return _unifiedNameAndTypeColumnsMap;
+            }
+        }
 
-        internal ThreadSafeDictionary<Signature, MostRecentlyUsedCache<CallSiteInfo, TypeSymbol>> CallSiteToResultTypeMap =
-            new ThreadSafeDictionary<Signature, MostRecentlyUsedCache<CallSiteInfo, TypeSymbol>>();
+        public ThreadSafeDictionary<IReadOnlyList<TableSymbol>, TableSymbol> CommonColumnsMap
+        {
+            get
+            {
+                if (_commonColumnsMap == null)
+                {
+                    Interlocked.CompareExchange(
+                        ref _commonColumnsMap,
+                        new ThreadSafeDictionary<IReadOnlyList<TableSymbol>, TableSymbol>(ReadOnlyListComparer<TableSymbol>.Default),
+                        null);
+                }
+                return _commonColumnsMap;
+            }
+        }
 
-        internal readonly ThreadSafeDictionary<Signature, FunctionBodyFacts> DatabaseFunctionBodyFacts =
-            new ThreadSafeDictionary<Signature, FunctionBodyFacts>();
+        public ThreadSafeDictionary<Signature, MostRecentlyUsedCache<CallSiteInfo, FunctionCallExpansion>> CallSiteToExpansionMap
+        {
+            get
+            {
+                if (_callSiteToExpansionMap == null)
+                {
+                    Interlocked.CompareExchange(
+                        ref _callSiteToExpansionMap,
+                        new ThreadSafeDictionary<Signature, MostRecentlyUsedCache<CallSiteInfo, FunctionCallExpansion>>(),
+                        null);
+                }
+                return _callSiteToExpansionMap;
+            }
+        }
+
+        public ThreadSafeDictionary<Signature, MostRecentlyUsedCache<CallSiteInfo, TypeSymbol>> CallSiteToResultTypeMap
+        {
+            get
+            {
+                if (_callSiteToResultTypeMap == null)
+                {
+                    Interlocked.CompareExchange(
+                        ref _callSiteToResultTypeMap,
+                        new ThreadSafeDictionary<Signature, MostRecentlyUsedCache<CallSiteInfo, TypeSymbol>>(),
+                        null);
+                }
+                return _callSiteToResultTypeMap;
+            }
+        }
+
+        public ThreadSafeDictionary<Signature, FunctionBodyFacts> DatabaseFunctionBodyFacts
+        {
+            get
+            {
+                if (_databaseFunctionBodyFacts == null)
+                {
+                    Interlocked.CompareExchange(
+                        ref _databaseFunctionBodyFacts,
+                        new ThreadSafeDictionary<Signature, FunctionBodyFacts>(),
+                        null);
+                }
+                return _databaseFunctionBodyFacts;
+            }
+        }
     }
 }
